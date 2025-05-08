f2f8e8b575e3d70c053d80e45940629a15c4945e
Caio Pizzol <caiopizzol@icloud.com>
2025-05-08 18:02:40 -0300

    chore: add package-lock.json and update package.json for npm compatibility, remove pnpm-lock.yaml

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -12,28 +12,27 @@ export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, '..');
 
     return new Promise<void>((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot })
-            .then((files: string[]) => {
-                // Add files to the test suite
-                files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
 
-                try {
-                    // Run the mocha test
-                    mocha.run((failures: number) => {
-                        if (failures > 0) {
-                            e(new Error(`${failures} tests failed.`));
-                        } else {
-                            c();
-                        }
-                    });
-                } catch (err) {
-                    console.error(err);
-                    e(err);
-                }
-            })
-            .catch((err: unknown) => {
+            // Add files to the test suite
+            files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.run((failures: number) => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
                 console.error(err);
                 e(err);
-            });
+            }
+        });
     });
 } 
